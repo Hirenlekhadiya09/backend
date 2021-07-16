804e34a399b6dfd32380adbd69c98c67825c610a
hiren <hirenlekhadiya@gmail.com>
2021-07-16 11:07:38 +0530

    changs in app file

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const app = express();
 require('dotenv').config();
 require('./src/config/conn')
 require('path')
+
+
+
 const signup = require('./src/Routes/signup');
 const addpost = require('./src/Routes/addpost')
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 4000
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:false}))
